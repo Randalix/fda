18d339a35bded7faecd36a6035f031af858016be
Randalix <j.heintz.90@gmail.com>
2022-08-08 17:17:21 +0200

    Added git commit to update fdas

--- git.py
+++ git.py
@@ -2,12 +2,23 @@ from subprocess import run
 from hou import ui
 
 def init(path):
-    commit_input = ui.readInput("Commit Message", buttons=('OK', 'Cancle'), initial_contents="inital commit")
     path = str( path.resolve() )
-    if commit_input[0] == 0:
+    run(["git",  "init"], cwd=path)
+    run(["git",  "add", "."], cwd=path)
+    run(["git",  "commit", "-m", "inital commit"], cwd=path)
+    run(["hub",  "create"], cwd=path)
+    run(["git",  "push", "--set-upstream", "origin", "master"], cwd=path)
+
+def update(path):
+    commit_input = ui.readInput("Commit Message", buttons=('OK', 'Cancle'))
+    path = str( path.resolve() )
+    if commit_input[0] == 0 and commit_input[1] != "":
         commit_message = commit_input[1]
-        run(["git",  "init"], cwd=path)
         run(["git",  "add", "."], cwd=path)
         run(["git",  "commit", "-m", commit_message], cwd=path)
-        run(["hub",  "create"], cwd=path)
-
+        run(["git",  "", "-m", commit_message], cwd=path)
+        run(["git",  "push", "--set-upstream", "origin", "master"], cwd=path)
+        return True
+    else:
+        ui.displayMessage("Commit Aborted", buttons=('OK'))
+        return False
